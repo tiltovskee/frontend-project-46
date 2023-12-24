d53d6d602ce48f920f07c29ae350ba017377719f
Max Moon <poriadoc@yandex.ru>
2023-12-24 16:00:43 +0300

    update test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,8 @@
 import { test, expect } from '@jest/globals';
-import genDiff from '../src/index.js';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { readFileSync } from 'node:fs';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,14 +10,33 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-const expectedStylish = readFile('result.txt');
+const expectedStylish = readFile('resultStylish.txt');
+const expectedPlain = readFile('resultPlain.txt');
+const expectedJson = readFile('resultJson.txt');
 
-test('genDiff json files', () => {
-  const pathToFile1 = getFixturePath('before.json');
-  const pathToFile2 = getFixturePath('after.json');
+const fileExtension = ['json', 'yml'];
+
+test.each(fileExtension)('Test gendiff for %s files', (extension) => {
+  const pathToFile1 = getFixturePath(`before.${extension}`);
+  const pathToFile2 = getFixturePath(`after.${extension}`);
   expect(genDiff(pathToFile1, pathToFile2)).toEqual(expectedStylish);
+  expect(genDiff(pathToFile1, pathToFile2, 'stylish')).toEqual(expectedStylish);
+  expect(genDiff(pathToFile1, pathToFile2, 'plain')).toEqual(expectedPlain);
+  expect(genDiff(pathToFile1, pathToFile2, 'json')).toEqual(expectedJson);
 });
 
+test('Wrong format', () => {
+  const pathToFile1 = getFixturePath('before.json');
+  const pathToFile2 = getFixturePath('after.json');
+  expect(() => {
+    genDiff(pathToFile1, pathToFile2, 'mp3');
+  }).toThrowError();
+});
 
-
-// Не забыть протестировать, что дефолтный формат - стайлиш
+test('Wrong file type', () => {
+  const pathToFile1 = getFixturePath('before.json');
+  const pathToFile2 = getFixturePath('resultJson.txt');
+  expect(() => {
+    genDiff(pathToFile1, pathToFile2, 'stylish');
+  }).toThrowError();
+});
