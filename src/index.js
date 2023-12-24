4b3f80eee79e38c2a39855e3dd8ef6a8fb3f30ee
Max Moon <poriadoc@yandex.ru>
2023-12-24 16:03:19 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ import path from 'node:path';
 import { cwd } from 'node:process';
 import { readFileSync } from 'node:fs';
 import parse from './parsers.js';
-import makeTree from './getDiff.js';
-import formatStylish from './formatters/stylish.js';
+import makeTree from './treeMaker.js';
+import getFormater from './formatters/index.js';
 
 const readFile = (pathToFile) => {
-  const fullPath = path.resolve(cwd(), '__fixtures__', pathToFile);
+  const fullPath = path.resolve(cwd(), pathToFile);
   const fileData = readFileSync(fullPath, 'utf-8');
   const fileExtension = path.extname(fullPath);
   return parse(fileData, fileExtension);
 };
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, outputFormat = 'stylish') => {
   const firstFile = readFile(filepath1);
   const secondFile = readFile(filepath2);
   const tree = makeTree(firstFile, secondFile);
-  return formatStylish(tree);
+  const formatDifference = getFormater(outputFormat);
+  return formatDifference(tree);
 };
 
 export default genDiff;
