743ad98fd376e66caefd0069ef2735d10e0b354b
Max Moon <poriadoc@yandex.ru>
2023-12-26 23:46:32 +0300

    update parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,14 @@
 import yaml from 'yaml';
 
-const parse = (data, fileType) => {
-  switch (fileType) {
-    case '.json':
+const parse = (data, format) => {
+  switch (format) {
+    case 'json':
       return JSON.parse(data);
-    case '.yaml':
-    case '.yml':
+    case 'yaml':
+    case 'yml':
       return yaml.parse(data);
     default:
-      throw new Error(`This utility doesn't support parsing ${fileType} files!`);
+      throw new Error(`This utility doesn't support parsing ${format} files!`);
   }
 };
 
