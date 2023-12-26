2465191c4362324560b23094256ca65f785587d0
Max Moon <poriadoc@yandex.ru>
2023-12-26 23:45:51 +0300

    update stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,12 +3,6 @@ import _ from 'lodash';
 const getIndent = (depth) => {
   const indent = ' ';
   const multiplier = 4;
-  return indent.repeat(depth * multiplier - 2);
-};
-
-const getIndentForBrackets = (depth) => {
-  const indent = ' ';
-  const multiplier = 4;
   return indent.repeat((depth - 1) * multiplier);
 };
 
@@ -17,38 +11,30 @@ const stringify = (value, depth) => {
     return `${value}`;
   }
   const entries = Object.entries(value);
-  const string = entries.map((pair) => `${getIndent(depth)}  ${pair[0]}: ${stringify(pair[1], depth + 1)}`);
-  return ['{', ...string, `${getIndentForBrackets(depth)}}`].join('\n');
+  const string = entries.map(([childKey, childValue]) => `${getIndent(depth)}    ${childKey}: ${stringify(childValue, depth + 1)}`);
+  return ['{', ...string, `${getIndent(depth)}}`].join('\n');
 };
 
 const toStylish = (tree) => {
   const iter = (node, depth) => {
     const data = node.map((item) => {
-      const {
-        key,
-        status,
-        children,
-        value,
-        valueBefore,
-        valueAfter,
-      } = item;
-      switch (status) {
+      switch (item.type) {
         case 'nested':
-          return `${getIndent(depth)}  ${key}: ${iter(children, depth + 1)}`;
+          return `${getIndent(depth)}    ${item.key}: ${iter(item.children, depth + 1)}`;
         case 'added':
-          return `${getIndent(depth)}+ ${key}: ${stringify(value, depth + 1)}`;
+          return `${getIndent(depth)}  + ${item.key}: ${stringify(item.value, depth + 1)}`;
         case 'deleted':
-          return `${getIndent(depth)}- ${key}: ${stringify(value, depth + 1)}`;
+          return `${getIndent(depth)}  - ${item.key}: ${stringify(item.value, depth + 1)}`;
         case 'unchanged':
-          return `${getIndent(depth)}  ${key}: ${stringify(value, depth + 1)}`;
+          return `${getIndent(depth)}    ${item.key}: ${stringify(item.value, depth + 1)}`;
         case 'changed':
-          return `${getIndent(depth)}- ${key}: ${stringify(valueBefore, depth + 1)}
-${getIndent(depth)}+ ${key}: ${stringify(valueAfter, depth + 1)}`;
+          return `${getIndent(depth)}  - ${item.key}: ${stringify(item.value1, depth + 1)}
+${getIndent(depth)}  + ${item.key}: ${stringify(item.value2, depth + 1)}`;
         default:
-          throw new Error(`Status ${status} not allowed!`);
+          throw new Error(`Type ${item.type} is not allowed!`);
       }
     });
-    return ['{', ...data, `${getIndentForBrackets(depth)}}`].join('\n');
+    return ['{', ...data, `${getIndent(depth)}}`].join('\n');
   };
   return iter(tree, 1);
 };
